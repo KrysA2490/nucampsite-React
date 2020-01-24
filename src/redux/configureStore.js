7dd66d60e8d91481e524c827d0dedb591eb5c083
KrysA19 <krystalokehie@gmail.com>
2020-01-24 13:50:30 -0800

    React Redux Forms Revisited

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,11 +1,12 @@
 import { createStore, combineReducers, applyMiddleware} from 'redux';
+import { createForms } from 'react-redux-form';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-
 import { Campsites } from './campsites';
 import { Comments } from './comments';
 import { Partners } from './partners';
 import { Promotions } from './promotions';
+import { InitialFeedback } from './forms';
 
 //create a store variable. Pass reducer function and initial state
 //createStore function requires reducer as argument - single reducer only!
@@ -15,7 +16,10 @@ export const ConfigureStore = () => {
             campsites: Campsites,
             comments: Comments,
             partners: Partners,
-            promotions: Promotions
+            promotions: Promotions,
+            ...createForms({
+                feedbackForm: InitialFeedback
+            })
         }),
 
         applyMiddleware(thunk, logger)
